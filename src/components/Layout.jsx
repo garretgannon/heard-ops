2c1e11d22c3d7a59080643450b987627d4747d5d
Garret Gannon <garretgannon@gmail.com>
2026-04-29 16:59:30 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -91,7 +91,7 @@ export default function Layout() {
           </div>
         </div>
 
-        <nav className="flex-1 px-3 space-y-1">
+        <nav className="flex-1 px-3 space-y-1 overflow-y-auto py-2">
           {navItems.map(item => (
             <Link
               key={item.path}
